78a6c47dc17fa9b34f61a11c48cb57e45bea81ff
POLINKAAAAAA1 <pakostusenko@edu.hse.ru>
2025-05-26 19:26:26 +0300

    Added beginning of function with 'if' check

--- cities.py
+++ cities.py
@@ -1,2 +1,19 @@
 cities = open("russian_cities.txt", "r", encoding="utf-8").read().split(", ")
 cities = [city.strip() for city in cities if city.strip()]
+
+
+def play_cities():
+  used cities = set()
+  availble_cities = [city.lower() for city in cities]
+  last_letter = None
+  While True:
+    user_city = input('ваш город: ').strip().lower()
+    if user_city == 'стоп':
+      print('игра окончена')
+      break
+    if user_city in used_cities:
+      print('этот город уже назван')
+      break
+    if user_city.title() not in cities:
+      print(f'город должен начинаться на букву {last_letter}')
+      break
